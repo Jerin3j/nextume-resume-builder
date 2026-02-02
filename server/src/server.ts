51f85848c1f474ca6d29fcb9448fc9119cf720ae
Jerin3J <jerinjj202@gmail.com>
2026-02-02 22:22:14 +0530

    fix(backend): fixed node error while run the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import prisma from "./prismaClient.js";
 import userRouter from "./routes/user.route.js";
 import resumeRouter from "./routes/resume.route.js";
+import aiRouter from "./routes/ai.route.js";
 
 const PORT: string | number = process.env.PORT || 3001;
 
@@ -19,6 +20,7 @@ app.get("/", async (_req, res) => {
 // Routes
 app.use("/api/users", userRouter);
 app.use("/api/resumes", resumeRouter);
+app.use("/api/ai", aiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
